3fe87bb605c44b3a85ea565facb4a8909f9bfc1a
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2024-06-10 08:19:16 +0530

    Time: 3 ms (11.65%), Space: 41 MB (90.77%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] sortedHeights = heights.clone();
+        bubbleSort(sortedHeights);
+        
+        int count = 0; 
+        
+        for(int i=0; i<sortedHeights.length; i++){
+            if(heights[i] != sortedHeights[i]) count++;
+        }
+        return count;
+    }
+    
+    public void bubbleSort(int[] arr){
+        int n = arr.length;
+        
+        for(int i=0; i<n-1; ++i){
+            for(int j=0; j<n-i-1; ++j){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
