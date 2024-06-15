623f7de74d8de4beb0a6ad6838d48b72803ad084
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2024-06-15 18:02:25 +0530

    Time: 76 ms (83.38%), Space: 63.2 MB (19.91%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -2,11 +2,12 @@ class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         PriorityQueue<Project> minPQ = new PriorityQueue<>();
         PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
+        
         int n = capital.length;
-        for(int i=0; i<n; i++){ // insert project in minPQ
-            minPQ.offer(new Project(profits[i], capital[i])); // ascending order sorting means min capital
-            // comes on top
+        for(int i=0; i<n; i++){
+            minPQ.offer(new Project(profits[i], capital[i]));
         }
+        
         while(k > 0){
             while(!minPQ.isEmpty() && minPQ.peek().capital <= w){
                 maxPQ.offer(minPQ.poll().profit);
@@ -17,16 +18,18 @@ class Solution {
         }
         return w;
     }
-}
-
-class Project implements Comparable<Project>{
-    int profit;
-    int capital;
-    Project(int profit, int capital){
-        this.profit = profit;
-        this.capital = capital;
-    }
-    public int compareTo(Project that){
-        return this.capital - that.capital; // sorted in increasing order of capital
+    
+    class Project implements Comparable<Project>{
+        int profit;
+        int capital;
+        
+        Project(int profit, int capital){
+            this.profit = profit;
+            this.capital = capital;
+        }
+        
+        public int compareTo(Project that){
+            return this.capital - that.capital;
+        }
     }
 }
