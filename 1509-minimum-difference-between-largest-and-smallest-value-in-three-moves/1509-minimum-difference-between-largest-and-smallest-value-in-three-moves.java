7ea4cafba641606f6a5d028ea0034e525c45086d
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2024-07-03 16:18:59 +0530

    Time: 15 ms (96.06%), Space: 57.1 MB (54.33%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,41 +1,17 @@
 class Solution {
     public int minDifference(int[] nums) {
-        int len = nums.length;
-        if(len <= 4) return 0;
+        int n = nums.length;
+        if(n <= 4) return 0;
         
-         // four smallest elements 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Arrays.sort(nums);
         
-        for(int num : nums){
-            maxHeap.offer(num);
-            if(maxHeap.size() > 4){
-                maxHeap.poll();
-            }
-        }
+        int res = Integer.MAX_VALUE;
         
-        List<Integer> smallestFour = new ArrayList<>(maxHeap);
-        Collections.sort(smallestFour);
+        res = Math.min(res, nums[n-4] - nums[0]);
+        res = Math.min(res, nums[n-3] - nums[1]);
+        res = Math.min(res, nums[n-2] - nums[2]);
+        res = Math.min(res, nums[n-1] - nums[3]);
         
-        // four largest elements 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        for(int num : nums){
-            minHeap.offer(num);
-            if(minHeap.size() > 4){
-                minHeap.poll();
-            }
-        }
-        
-        List<Integer> largestFour = new ArrayList<>(minHeap);
-        Collections.sort(largestFour);
-        
-        int minDiff = Integer.MAX_VALUE;
-        // four scenarios 
-        for(int i=0; i<4; i++){
-            minDiff = Math.min(minDiff, largestFour.get(i) - smallestFour.get(i));
-        }
-        
-        return minDiff;
-        
+        return res;
     }
 }
