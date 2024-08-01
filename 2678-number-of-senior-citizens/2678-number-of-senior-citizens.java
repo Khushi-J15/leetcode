8d9e62eda33ee44fa73ee1cbde924dea16c0683c
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2024-08-01 13:00:25 +0530

    Time: 1 ms (82.64%), Space: 42.8 MB (21.29%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        
+        for(int i=0; i<details.length; i++){
+            String s = details[i].substring(11,13);
+            if(Integer.parseInt(s) > 60){
+                count++;
+            }
+            else{
+                continue;
+            }
+        }
+        
+        return count;
+    }
+}
