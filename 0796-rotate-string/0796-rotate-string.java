89f20be1082f820e51a346296c304fbb08ae710d
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2024-11-03 10:04:09 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (27.67%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,8 +2,21 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length() != goal.length()) return false;
         
-        String doubledString = s + s;
+        int length = s.length();
+        char[] sChars = s.toCharArray(); // 'a', 'b'
         
-        return (doubledString.contains(goal));
+        for(int i=0; i<length; i++){
+            sChars = rotateOnce(sChars);
+            if(new String(sChars).equals(goal)) return true;
+        }
+        
+        return false;
+    }
+    
+    private char[] rotateOnce(char[] arr){
+        char firstChar = arr[0];
+        System.arraycopy(arr, 1, arr, 0, arr.length - 1);
+        arr[arr.length - 1] = firstChar;
+        return arr;
     }
 }
