6554a7a4f5bdd6cf375d2a1e48a0f160205419fd
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2024-11-26 22:19:28 +0530

    Time: 1 ms (100.00%), Space: 46 MB (25.00%) - LeetHub

---
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+         int[] indegree = new int[n];
+            
+        for (int[] edge : edges) {
+            indegree[edge[1]]++;
+        }
+        
+        int champ = -1;
+        int champCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            // If the team can be a champion, store the team number and increment the count
+            if (indegree[i] == 0) {
+                champCount++;
+                champ = i;
+            }
+        }
+        
+        return champCount > 1 ? -1 : champ;
+    }
+}
